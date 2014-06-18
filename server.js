2b309cdb85bab0fd9359300eff2f3b1fae7fe537
stanzheng <dev.stanzheng@gmail.com>
2014-06-18 01:11:58 -0400

    setting content type

--- server.js
+++ server.js
@@ -15,7 +15,24 @@ app.get('/', function(req, res){
         console.log('Done');
         var converted = JSON.stringify(result, undefined, 2);
         console.log(converted);
+        res.set('Content-Type', 'application/json');
         res.send(converted);
+        });
+      }
+    });
+});
+
+app.get('/pretty', function(req, res){
+  request('http://www.norfolkva.gov/cultural_affairs/public_art_downtown.xml',
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        //console.log(body); // Print the google web page.
+        parser.parseString(body, function (err, result) {
+        console.log('Done');
+        var converted = JSON.stringify(result, undefined, 2);
+        console.log(converted);
+        res.set('Content-Type', 'application/json');
+        res.send( "<pre>" + converted + "</pre>");
         });
       }
     });
