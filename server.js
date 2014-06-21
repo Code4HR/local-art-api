7d2df19c7017072b43c968ba6a611e47f1634faa
stanzheng <dev.stanzheng@gmail.com>
2014-06-21 00:54:43 -0400

    adding a new route

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var xml2js = require('xml2js');
 var request = require('request');
 var express = require('express');
+var _ = require('lodash');
 var cors = require('cors');
 var app = express();
 
@@ -36,6 +37,21 @@ app.get('/pretty', function(req, res){
         console.log(converted);
         res.set('Content-Type', 'application/json');
         res.send( "<pre>" + converted + "</pre>");
+        });
+      }
+    });
+});
+
+app.get('/exhibits/:exhibit', function(req, res){
+  request('http://www.norfolkva.gov/cultural_affairs/public_art_downtown.xml',
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        //console.log(body); // Print the google web page.
+        parser.parseString(body, function (err, result) {
+        var converted = JSON.stringify(result, undefined, 2);
+        console.log(converted);
+        res.set('Content-Type', 'application/json');
+        res.send(converted);
         });
       }
     });
