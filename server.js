546bd988182b7e6a687b11890fb888611a30d75f
stanzheng <dev.stanzheng@gmail.com>
2014-06-25 10:21:05 -0400

    wow yeah occums razor

--- server.js
+++ server.js
@@ -12,30 +12,11 @@ app.use(cors());
 
 
 //var Art = require('./model/exhibit.js');
-var Art = require('./data/exhibits.json');
+var Exhibit = require('./data/exhibits.json');
 
 app.get('/', function(req, res){
   res.redirect(301, '/exhibits');
  });
-
-app.get('/pretty', function(req, res){
-  request('http://www.norfolkva.gov/cultural_affairs/public_art_downtown.xml',
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        parser.parseString(body, function (err, result) {
-        var converted = JSON.stringify(result, undefined, 2);
-        //print pretty
-        console.log(converted);
-        res.set('Content-Type', 'application/json');
-        res.send( "<pre>" + converted + "</pre>");
-        });
-      }
-      else {
-        res.send(404,"Not found");
-      }
-    });
-});
-
 
 //exhibits
 /* Query Strings to search for ...
@@ -47,42 +28,16 @@ app.get('/pretty', function(req, res){
     longitude: DS.attr('number'),
 */
 app.get('/exhibits', cors(), function(req, res){
-  request('http://www.norfolkva.gov/cultural_affairs/public_art_downtown.xml',
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var obj = [];
-        parser.parseString(body, function (err, result) {
-        _.each(result.parks.parkz, function (data) {
-          obj.push(data.$);
-        });
-          res.set('Content-Type', 'application/json');
-          res.send(obj);
-        });
-      }
-      else {
-        res.send(404,"Not found");
-      }
-    });
+  res.set('Content-Type', 'application/json');
+  res.send(200,Exhibit);
 });
 
 app.get('/exhibits/:id', cors(), function(req, res){
   //parm id
   var id = req.params.id;
-  request('http://www.norfolkva.gov/cultural_affairs/public_art_downtown.xml',
-    function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-          var obj;
-          parser.parseString(body, function (err, result) {
-          console.log(result.parks.parkz);
-          obj = result.parks.parkz[id].$;
-          res.set('Content-Type', 'application/json');
-          res.send(obj);
-        });
-      }
-      else {
-        res.send(404,"Not found");
-      }
-    });
+  res.set('Content-Type', 'application/json');
+  res.send(200,Exhibit[id]);
+
 });
 
 var port = Number(process.env.PORT || 5555);
